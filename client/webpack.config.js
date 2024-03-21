891133d5958c72e02550f68c1b2bcd4e0887b932
cbursch13 <cameron.bursch@target.com>
2024-03-21 18:20:11 -0500

    bug fixes for inject manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = () => {
         ],
       }),
       new InjectManifest({
-        swSrc: './sw.js',
+        swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
      
